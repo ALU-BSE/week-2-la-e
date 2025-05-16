7f58b10738d44ab8c12f72c444c043c7410ae0ac
Enzo Bat <e.batungwan@alustudent.com>
2025-05-16 13:35:48 -0400

    created the views

--- pagination_project/books/views.py
+++ pagination_project/books/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from djangocore.paginator import Paginator
+from .models import Book
 
 # Create your views here.
+def book_list(request):
+    books= Book.objects.all().order_by('title')
+    Paginator = Paginator(books, 5)
+    
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    
+    return render(request, 'books/book_list.html', {'page_obj': page_obj})
