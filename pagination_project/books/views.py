8b15bb8605d9c864db1d8e752a894fda4831751c
Enzo Bat <e.batungwan@alustudent.com>
2025-05-16 14:14:35 -0400

    created template and fixed some naming error that caused bugs

--- pagination_project/books/views.py
+++ pagination_project/books/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
-from djangocore.paginator import Paginator
+from django.core.paginator import Paginator
 from .models import Book
 
 # Create your views here.
 def book_list(request):
     books= Book.objects.all().order_by('title')
-    Paginator = Paginator(books, 5)
+    paginator = Paginator(books, 5)
     
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
